f93fe96beef4eeb03a161638802f597383101018
Michael Kim <m.kim4247@gmail.com>
2018-09-22 06:33:16 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(n)
-  if n % 3 == 0
+  if n % 5 == 0 && n % 3 == 0 
+    return "FizzBuzz"
     return "Fizz"
   elsif n % 5 == 0 
     return "Buzz"
-  elsif n % 5 == 0 && n % 3 == 0 
-    return "FizzBuzz"
+  elsif 
   else 
     return nil 
   end
